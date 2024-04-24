85c57af5ff3d519d1394d8b54c9a468c33dacea1
Ryan King <ryan@thedevhouseagency.com>
2024-04-24 02:02:46 -0500

    Attempt at colored hierarchy icons
    Unable to get the icon to render properly. Tried replacing pixels and creating a shader.

--- Assets/External_Packages/CGHierarchyIcons/Editor/UnityCallbacks/ShowIconOnHierarchyView.cs
+++ Assets/External_Packages/CGHierarchyIcons/Editor/UnityCallbacks/ShowIconOnHierarchyView.cs
@@ -8,9 +8,21 @@ namespace CGHierarchyIconsEditor
     public class ShowIconOnHierarchyView
     {
         const float MAX_ICON_SIZE = 16;
+        //static Material tintMat;
 
         static ShowIconOnHierarchyView()
         {
+            ////Shader shade = Shader.Find("Shader Graphs/IconTint");
+            ////if(shade == null)
+            ////{
+            ////    Debug.LogError("Could not find shader named: " + "Shader Graphs/IconTint");
+            ////}
+            ////else
+            ////{
+            ////    Debug.Log("Shader found: " + shade.name);
+            ////}
+
+            //tintMat = new Material(shade);
             EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemCallback;
         }
 
@@ -34,9 +46,27 @@ namespace CGHierarchyIconsEditor
                 else
                     x += (selectionRect.width - width) - hierarchyIcon.position * MAX_ICON_SIZE;
 
-                // draw the icon
+                //draw the icon
                 Rect rect = new Rect(x, selectionRect.y, width, height);
                 GUI.DrawTexture(rect, icon);
+
+
+
+
+                //// Convert screen coordinates to GUI space coordinates
+                //Vector2 guiPosition = EditorGUIUtility.GUIToScreenPoint(new Vector2(x, selectionRect.y));
+                //Rect rect = new Rect(guiPosition.x, guiPosition.y, width, height);
+
+                //// Set the texture and color properties of the material
+                //tintMat.SetTexture("_MainTexture", icon);
+                //tintMat.SetColor("_TintColor", Color.blue);
+
+                //// Draw the Quad with the material
+                //GUI.DrawTexture(rect, EditorGUIUtility.whiteTexture, ScaleMode.ScaleToFit, true, 0, Color.blue, 0, 0);
+                //Graphics.DrawTexture(rect, EditorGUIUtility.whiteTexture, tintMat);
+
+
+
 
                 // set link cursor
                 EditorGUIUtility.AddCursorRect(rect, MouseCursor.Link);
@@ -44,6 +74,23 @@ namespace CGHierarchyIconsEditor
                 // and draw a button for change the icon and display the tooltip
                 if (GUI.Button(rect, new GUIContent(string.Empty, hierarchyIcon.tooltip), EditorStyles.label))
                     PopupWindow.Show(rect, new PickIconWindow(hierarchyIcon));
+
+
+                //Texture2D icon = hierarchyIcon.icon ? hierarchyIcon.icon : TextureHelper.NO_ICON;
+
+                //// Calculate the position based on the direction and icon size
+                //float x = selectionRect.x;
+                //if (hierarchyIcon.direction == HierarchyIcon.Direction.RightToLeft)
+                //    x += selectionRect.width - MAX_ICON_SIZE - hierarchyIcon.position * MAX_ICON_SIZE;
+                //else
+                //    x += hierarchyIcon.position * MAX_ICON_SIZE;
+
+                //// Convert screen coordinates to GUI space coordinates
+                //Vector2 guiPosition = EditorGUIUtility.GUIToScreenPoint(new Vector2(x, selectionRect.y));
+                //Rect rect = new Rect(guiPosition.x, guiPosition.y, MAX_ICON_SIZE, MAX_ICON_SIZE);
+
+                //// Draw the icon using the material
+                //Graphics.DrawTexture(rect, icon, tintMat);
             }
         }
     }
